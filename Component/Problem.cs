b8a8564bb56691a15c5a47e44bbe42ddb15dfe6b
bhanu03042005 <bvchary345@gmail.com>
2026-03-18 18:25:14 +0530

    Add Problem class with properties

---
+++ Component/Problem.cs
@@ -0,0 +1,15 @@
+ namespace ASPNET6_Template.Models
+{
+    public class Problem
+    {
+        //Implement your code here
+        public int Id { get; set; }
+        public string InterrogatorsProblem { get; set; }
+        public string Choice1 { get; set; }
+        public string Choice2 { get; set; }
+        public string Choice3 { get; set; }
+        public string Choice4 { get; set; }
+        public string Solution { get; set; }
+    }
+}
+
